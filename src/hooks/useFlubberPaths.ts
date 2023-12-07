3a3c0a6bd4fbe882f9ca01d1f74ef3f5980580f0
BraveLilToaster <penguindude2691@gmail.com>
2023-12-07 00:39:32 -0600

    Remove React components and optimized 'me' images

--- src/hooks/useFlubberPaths.ts
+++
@@ -1,7 +0,0 @@
-import { MotionValue, useTransform } from "framer-motion";
-
-export const getIndex = (_: any, index: number) => index;
-
-export function useFlubber(progress: MotionValue<number>, paths: string[]) {
-  return useTransform(progress, paths.map(getIndex), paths);
-}
